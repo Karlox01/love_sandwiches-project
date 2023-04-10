910400955cd8610acc36a53f967c19ca5fe03672
Karlox01 <orlandinikarlo@gmail.com>
2023-04-10 18:15:38 +0000

    complete project 3

--- second.py
+++ second.py
@@ -1,18 +1,35 @@
 import random
 
-print('Welcome User!')
-print('*-*-*-*-*-*-*-*-*-*-*-*-*-*-')
+print(""" 
+Welcome HUMBLE USER , WE HOPE YOU WILL ENJOY THE ...!
+""")
+print(""" 
+     _____  _  ____     _____  ____  ____     _____  ____  _____         
+    /__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/ 
+      / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \ 
+      | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_   
+      \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\ 
+
+
+""")
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+tie_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 field = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 horizontal = 3
 vertical = 3
 
 
-choose = input('\n Do you wish to proceed with the tic tac toe game? :').lower()
 
 
 def displayField():
+
+
+  """ 
+  A function that sets the field that iterates through the field
+  Sets the boundaries and field
+
+  """ 
   for x in range(horizontal):
     print('\n+---+---+---+')
     print('|', end='')
@@ -23,8 +40,11 @@ def displayField():
 
 
 
-
 def inputInArray(num, turn):
+  """ 
+  Function that identifies the field against the players choice
+  Will be later utilized in the main function 
+  """
   num -= 1
   if (num == 0):
     field[0][0] = turn
@@ -47,7 +67,8 @@ def inputInArray(num, turn):
 
 
 def checkWhoWon(field):
-
+  # A function that indentifies the winner of the game Includes X Axis Y Axis and Horizontal
+  
   ### X Axis
   if (field[0][0] == 'X' and field[0][1] == 'X' and field[0][2] == 'X'):
     print('X has won!')
@@ -103,41 +124,65 @@ def checkWhoWon(field):
     
 
 
-leaveLoop = False
-turn = 'X'
-turnCounter = 0
 
 
-if (choose == 'yes'):
-  while (leaveLoop == False):
-    if (checkWhoWon(field) == True):
-      break
+def main_function():
+  
+  """ 
+  This is the main function that runs the game , Iterates/counts between the player turns,
+  And determines the winner or a tie.
 
-  ### Its the player turn
-    if (turnCounter % 2 == 1):
-      displayField()
-      chosenNumber = int(input('\n chose a number between 1 and 9 : '))
-      if (chosenNumber >= 1 and chosenNumber <= 9 and chosenNumber != aiChoice):
-        inputInArray(chosenNumber, 'X')
-        numbers.remove(chosenNumber)
-        checkWhoWon(field)
-        turnCounter += 1
-      else:
-        print('Invalid input number, Please try again , Numbers 1 - 9 ONLY!****')
-      
+  """ 
 
-  ### Competitors turn
-    else:
-      while (True):
-        aiChoice = random.choice(numbers)
-        print('\n Computer has chosen: ', aiChoice)
-        if (aiChoice in numbers):
-          inputInArray(aiChoice, 'O')
-          numbers.remove(aiChoice)
+  leaveLoop = False
+  turn = 'X'
+  turnCounter = 0
+  choose = input('\n Do you wish to proceed? ... \n Or are you afrid the computer will best you! : ').lower()
+
+  if (choose == 'yes' or choose == 'yea'):
+    while (leaveLoop == False):
+      if (checkWhoWon(field) == True): 
+        displayField()        
+        break
+      elif (turnCounter == 9 and checkWhoWon(field) != True): # Sets the tie if there is no winner after turn 9 
+        print(""" 
+                   ~~ This is a tie! ~~ 
+     _____  _  ____     _____  ____  ____     _____  ____  _____         
+    /__ __\/ \/   _\   /__ __\/  _ \/   _\   /__ __\/  _ \/  __/ 
+      / \  | ||  / _____ / \  | / \||  / _____ / \  | / \||  \ 
+      | |  | ||  \_\____\| |  | |-|||  \_\____\| |  | \_/||  /_   
+      \_/  \_/\____/     \_/  \_/ \|\____/     \_/  \____/\____\ 
+
+        """ ) 
+        break
+
+    ### Its the player turn
+      if (turnCounter % 2 == 1): # Determines cpu goes first 
+        displayField()
+        chosenNumber = int(input('\n chose a number between 1 and 9 : '))
+        if (chosenNumber >= 1 and chosenNumber <= 9 and chosenNumber != aiChoice): #Makes sure player can not pick the number already picked by CPU,This game me a headache but was such a simple solution
+          inputInArray(chosenNumber, 'X')
+          numbers.remove(chosenNumber)
           turnCounter += 1
-          break
-else:
-  print('Oh never mind than play once u feel like it :)')
+        else:
+          print('Invalid input number, Please try again , Numbers 1 - 9 ONLY!\nDO NOT copy already used numbers :) ')
+
+    ### Competitors turn
+      else:
+        while (True):
+          aiChoice = random.choice(numbers)
+          print('\n Computer has chosen: ', aiChoice)
+          if (aiChoice in numbers):
+            inputInArray(aiChoice, 'O')
+            numbers.remove(aiChoice)
+            turnCounter += 1
+            break
+  
+  else:
+    print('Oh never mind , Come again if you change your mind! :) ')
+  
+
+main_function()
 
 
 
